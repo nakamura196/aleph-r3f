3e4baefc2c9b0b9dba9cc11b59928379903d1424
Edward Silverton <e.silverton@gmail.com>
2024-02-19 16:43:02 +0000

    isVercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
 
+// Check if the build is running on Vercel
+const isVercel = process.env.VERCEL === '1';
+
 export default defineConfig({
   plugins: [react(), dts()],
   resolve: {
@@ -10,25 +13,52 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'index.ts'),
-      name: 'Aleph',
-      fileName: (format) => `index.${format}.js`,
-    },
-    cssCodeSplit: false,
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
+  build: isVercel
+    ? {
+        outDir: 'dist', // output build files to 'dist' directory
+        emptyOutDir: true, // empty the output directory before build
+        rollupOptions: {
+          input: resolve(__dirname, 'index.html'), // entry point for the application
         },
+      }
+    : {
+        lib: {
+          entry: resolve(__dirname, 'index.ts'),
+          name: 'Aleph',
+          fileName: (format) => `index.${format}.js`,
+        },
+        cssCodeSplit: false,
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
       },
-    },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
+  // build: {
+  //   lib: {
+  //     entry: resolve(__dirname, 'index.ts'),
+  //     name: 'Aleph',
+  //     fileName: (format) => `index.${format}.js`,
+  //   },
+  //   cssCodeSplit: false,
+  //   rollupOptions: {
+  //     external: ['react', 'react-dom'],
+  //     output: {
+  //       globals: {
+  //         react: 'React',
+  //         'react-dom': 'ReactDOM',
+  //       },
+  //     },
+  //   },
+  //   sourcemap: true,
+  //   emptyOutDir: true,
+  // },
   server: {
     port: 3000,
   },
